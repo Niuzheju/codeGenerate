2536319ba32eeab9f7934b4de4a57e49892810b5
tom <1523892672@qq.com>
2019-01-06 19:56:08 +0800

    ok

--- src/main/java/com/niuzj/main/Test.java
+++ src/main/java/com/niuzj/main/Test.java
@@ -1,39 +1,39 @@
 package com.niuzj.main;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Test {
 
     private static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        String model = null;
-        String rootPath = null;
-        String rootPackagePath = null;
-        String controllerPath = null;
-        String jspPath = null;
+    public static void main(String[] args) throws IOException {
+        String model = "com.niuzj.admin.model.User";
+        String rootPath = "F:\\技术学习\\testpro\\src\\main";
+        String rootPackagePath = "com.niuzj.admin";
+        String controllerPath = "/user/user_manage.htm";
+        String jspPath = "/user/user_manage.jsp";
+        String table = "user";
 
-        try {
-            System.out.println("输入实体类全类名:");
-            model = scanner.nextLine();
-            System.out.println("输入模块所在目录:");
-            rootPath = scanner.nextLine();
-            System.out.println("输入模块所在包:");
-            rootPackagePath = scanner.nextLine();
-            System.out.println("输入controller访问路径:");
-            controllerPath = scanner.nextLine();
-            System.out.println("输入jsp所在路径:");
-            jspPath = scanner.nextLine();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        CodeGenerator generator = new CodeGenerator(model, rootPath, rootPackagePath, controllerPath, jspPath);
+//        try {
+//            System.out.println("输入实体类全类名:");
+//            model = scanner.nextLine();
+//            System.out.println("输入main所在目录:");
+//            rootPath = scanner.nextLine();
+//            System.out.println("输入基础包:");
+//            rootPackagePath = scanner.nextLine();
+//            System.out.println("输入表名");
+//            table = scanner.nextLine();
+//            System.out.println("输入controller访问路径:");
+//            controllerPath = scanner.nextLine();
+//            System.out.println("输入jsp所在路径:");
+//            jspPath = scanner.nextLine();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        CodeGenerator generator = new CodeGenerator(model, rootPath, rootPackagePath, controllerPath, jspPath, table);
         generator.init();
-//        System.out.println(model);
-//        System.out.println(rootPath);
-//        System.out.println(rootPackagePath);
-//        System.out.println(controllerPath);
-//        System.out.println(jspPath);
+        generator.generate();
 
     }
 
